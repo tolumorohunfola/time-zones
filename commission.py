9dd74fcc02530c2d313ef72a3a61decedfde6a46
tolumorohunfola <111201704+tolumorohunfola@users.noreply.github.com>
2024-01-03 19:54:12 +0000

    Set out what I've commissioned for myself within the commission.pyfile

---
+++ commission.py
@@ -0,0 +1,8 @@
+# This is a program to determine the timezone using your location (automatically or manually)
+
+# It should then request what the timezone you would like to see is
+
+# It should then translate the time into the requested timezone. It should also be able to project what the time will be for the timezone you request if the time is input in your current timezone.
+
+# Extension - compare multiple timezones.
+
